0bc469852952d0faec15cb7e1a10d5f7b88f1afe
Sebouh Paul <sebouh.paul@gmail.com>
2022-11-13 23:31:53 -0500

    updated distortion.py

--- src/distortion.py
+++ src/distortion.py
@@ -14,11 +14,18 @@ class AdaptiveClip:
 class AdaptiveCubic:
     def __init__(self):
         self.name="adaptive_cubic"
-        self.parameters=dict(a=0.1)
+        self.parameters=dict(a=0.5)
         self.parameter_types=dict(a=float)
     def apply_effect(self, indata, outdata):
         a = self.parameters['a']
         rms=np.std(indata)
 
-        a*=1/max(.001,rms**2)
-        outdata[:] = indata-a*indata**3
+        a*=1/rms**2
+
+
+        #x-a x^3
+        #1-3 a x^2
+        x=np.sqrt(1/(3*a))
+        c = x-a*x**3
+        
+        outdata[:] = (indata-a*indata**3)*(indata**2<1/(3*a))+(indata**2>=1/(3*a))*c*np.sign(indata)
